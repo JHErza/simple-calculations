0d948d589fd73eb046c141f937c03f425e7b842e
Route9Library <Jhmediaatworks@gmail.com>
2020-07-29 16:33:13 -0400

    js

---
+++ calcs.js
@@ -0,0 +1,25 @@
+// console.log("Code Differently");
+
+// console.log(12+30);
+
+// console.log(3+4);
+
+// console.log(3-5);
+
+// console.log(3*3);
+
+// console.log(12/2);
+
+var x = 72;
+var y = 8;
+
+// console.log(x + y);
+
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+
+
+for (var i=5; i<15; i += 2){
+    console.log(i);
+}
