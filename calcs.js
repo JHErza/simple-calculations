c7af33103d5b26b7d3f6b58cd33e6a5373c6b45f
Route9Library <Jhmediaatworks@gmail.com>
2020-07-30 16:29:36 -0400

    learn eles if part of js

--- calcs.js
+++ calcs.js
@@ -1,25 +1,72 @@
-// console.log("Code Differently");
 
-// console.log(12+30);
 
-// console.log(3+4);
+// var x = 72;
+// var y = 8;
 
-// console.log(3-5);
+// // console.log(x + y);
 
+// // console.log(x - y);
+// // console.log(x * y);
+// // console.log(x / y);
+
+
+// for (var i=5; i<15; i += 2){
+//     console.log(i);
+// }
+
+
+// var firstName = "Jameka";
+
+// var lastName = "Harmon";
+
+// var word ="Film Making";
+
+// console.log("Hi, my name is " + firstName +" "+ lastName + "." +" And my dream job is " + word);
+
+// console.log("Avatar");
 // console.log(3*3);
-
+// console.log(3-5);
 // console.log(12/2);
 
-var x = 72;
-var y = 8;
+// var num=10;
+// var num2=2;
 
-// console.log(x + y);
+// console.log(num + num2);
+// console.log(num - num2);
+// console.log(num * num2);
+// console.log(num/num2);
 
-// console.log(x - y);
-// console.log(x * y);
-// console.log(x / y);
+// for (var i=1; i<=5; i++){
+//     console.log(i);
+// }
+
+// for (var i=15; i>=5; i-=2){
+//     console.log(i);
+// }
+
+// var firstName = "Jameka ";
+// var lastName = "Harmon";
+
+// var fullName = firstName.concat(lastName);
+
+// console.log(fullName)
+
+// Writes a program that print 1-100 Multiples of 3 print "Fizz" Multiples of 5 print "Buzz"
+
+// for(var i =5; i<=15; i++){
+//     if(i > 7){
+//         console.log("less than");
+//     } else{
+//         console.log("greater than");
+//     }
+// }
 
 
-for (var i=5; i<15; i += 2){
-    console.log(i);
+for(var i=1; i<=100; i++){
+    if(i % 3 == 0){
+        console.log("Fuzz")
+    }else{
+        console.log("cool coders")
+    }
 }
+
