94f69c918f276efce6980dea24c7e001df25a129
Route9Library <Jhmediaatworks@gmail.com>
2020-08-07 13:59:36 -0400

    going through code

--- Array2.js
+++ Array2.js
@@ -1,1 +1,13 @@
-var color ("red", "yellow")
+var color ("red" "yellow" "green" "blue" "pink"){
+    console.log()
+}
+var str1:"red";
+var str2:"yellow";
+var str3:"green";
+var str4:"blue";
+var str5:"pink";
+
+
+
+var fourthColor //equals to the 4th color in the list
+
